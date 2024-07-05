9e07df3bd04abfd0ef47dc53a155dd957aca6c86
Man-Chun Yu <brian0821yu@gmail.com>
2024-07-05 17:22:17 +0100

    Add button to reset and adjust number of grids

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-let numberOfGrid = 16;
 const container = document.querySelector('.container');
+const resetButton = document.getElementById('reset')
 
+resetButton.addEventListener('click', () => {
 
-for (let i = 0; i < numberOfGrid; i++){
-    const rowdiv = document.createElement('div');
-    rowdiv.classList.add('rowdiv');
-    for (let j = 0; j < numberOfGrid; j++){
-        const grid = document.createElement('div');
-        grid.classList.add('grid');
-        rowdiv.appendChild(grid);
+    let newGridNumber = 0;
+    
+    do {
+        newGridNumber = prompt('Number?');
+    } while (newGridNumber > 100);
+
+    const rowdivs = document.querySelectorAll('.rowdiv');
+    for (const rowdiv of rowdivs){
+        container.removeChild(rowdiv);  
     }
-    container.appendChild(rowdiv);
+    setGrids(newGridNumber);
+});
+
+function setGrids(numberOfGrid) {
+    for (let i = 0; i < numberOfGrid; i++){
+        const rowdiv = document.createElement('div');
+        rowdiv.classList.add('rowdiv');
+        for (let j = 0; j < numberOfGrid; j++){
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            rowdiv.appendChild(grid);
+        }
+        container.appendChild(rowdiv);
+    }
+
+    container.addEventListener('mouseover', event => {
+        event.target.style.backgroundColor = 'black';
+    });
 }
 
-
-container.addEventListener('mouseover', event => {
-    event.target.style.backgroundColor = 'black';
-      
-});
+setGrids(16);
